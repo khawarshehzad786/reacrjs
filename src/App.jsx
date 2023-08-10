dfd18504303d0780bb56b547db144aa773298998
Khawar Shehzad <khawarshehzad856@gmail.com>
2023-08-10 22:54:44 +0500

    table

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import './App.css';
+import { useDownloadExcel } from 'react-export-table-to-excel'
 const App = () => {
+  const tableref=useRef(null)
+  
+  const data = [{
+    id:1,
+    name:"Khawar",
+    email:"Khawar@gmail.com"
+  },
+  {
+    id:2,
+    name:"Shehzad",
+    email:"shehzad@gmail.com"
+  },
+  ]
+  const {onDownload}=useDownloadExcel({
+    currentTableRef:tableref.current,
+    filename:'excelsheet',
+    sheet:'userData'
+  })
   return (
-    <div>App</div>
+    <>
+<h1>React Table</h1>
+<button style={{backgroundColor:"blue",color:"#fff"}} onClick={onDownload}>Export Excel</button>
+<table 
+ref={tableref}
+>
+  <thead >
+    <td>id</td>
+    <td>name</td>
+    <td>email</td>
+  </thead>
+  <tbody>
+ 
+      {
+        data.map((ks)=>{
+          return(
+            <tr>
+                <td>{ks.id}</td>
+                <td>{ks.name}</td>
+                <td>{ks.email}</td>
+
+            </tr>
+          )
+        })
+      }
+
+  </tbody>
+</table>
+
+    </>
   )
 }
 
